7284d2a09a66749ddda3bad9bdffe45b4e47e8bf
Malik Enes Safak <e.maliksafak@gmail.com>
2025-11-20 22:42:19 +0300

    Update

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763660878|4400525342';
+const CACHE_VERSION = '1763667717|27230062';
 /** @type {string} */
 const CACHE_PREFIX = 'Hive-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
